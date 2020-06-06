8f6c76f012a6ed3bb55a0d084ddd5d4ec4b0c6c1
Conrad Taylor <conradwt@gmail.com>
2020-06-06 01:29:25 -0700

    Refactor the creation of associated friends.

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,14 +21,14 @@ matz = Person.create!(first_name: 'Yukihiro',
                       email: 'matz@heroku.com',
                       username: 'matz')
 
-me.friendships.create!(person_id: me.id, friend_id: matz.id)
+me.friendships.create!(friend_id: matz.id)
 
-dhh.friendships.create!(person_id: dhh.id, friend_id: ezra.id)
-dhh.friendships.create!(person_id: dhh.id, friend_id: matz.id)
+dhh.friendships.create!(friend_id: ezra.id)
+dhh.friendships.create!(friend_id: matz.id)
 
-ezra.friendships.create!(person_id: ezra.id, friend_id: dhh.id)
-ezra.friendships.create!(person_id: ezra.id, friend_id: matz.id)
+ezra.friendships.create!(friend_id: dhh.id)
+ezra.friendships.create!(friend_id: matz.id)
 
-matz.friendships.create!(person_id: matz.id, friend_id: me.id)
-matz.friendships.create!(person_id: matz.id, friend_id: ezra.id)
-matz.friendships.create!(person_id: matz.id, friend_id: dhh.id)
+matz.friendships.create!(friend_id: me.id)
+matz.friendships.create!(friend_id: ezra.id)
+matz.friendships.create!(friend_id: dhh.id)
