38cd7e923b4b75d9fdf7b26f9a4444d86b36875d
Conrad Taylor <conradwt@gmail.com>
2022-09-13 18:04:52 -0700

    Refresh Ruby gems.

--- dev.entrypoint.sh
+++ dev.entrypoint.sh
@@ -4,10 +4,25 @@ set -e
 
 echo "Environment: $RAILS_ENV"
 
-# install missing gems
+#
+# https://bundler.io/guides/bundler_docker_guide.html
+#
+
+# You will also need to unset BUNDLE_PATH and BUNDLE_BIN. Unsetting environment
+# variables can be somewhat tricky in Docker, but the most common way is at the
+# beginning of your ENTRYPOINT script:
+unset BUNDLE_PATH
+unset BUNDLE_BIN
+
+# for development only:
+#   if the gems are installed, skip bundle.
+#   if not, install them.
 bundle check || bundle install --jobs 20 --retry 5
 
-# remove pre-existing puma/passenger server.pid
+# run any pending migrations
+bin/rails db:migrate
+
+# remove pre-existing puma or passenger server.pid
 rm -f ${APP_PATH}/tmp/pids/server.pid
 
 # run passed commands
