2519d1a755cb68d3f5e9c96cbfeb7bd349a31012
Conrad Taylor <conradwt@gmail.com>
2023-03-15 21:39:43 -0700

    Allow test environment to be able to access GraphiQL.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  if Rails.env.development?
+  if Rails.env.development? or Rails.env.test?
     mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
   end
 
