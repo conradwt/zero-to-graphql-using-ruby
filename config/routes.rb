dbbd435d18879ecfdbeffddd5c45783ac99e995e
Conrad Taylor <conradwt@gmail.com>
2025-03-11 20:13:31 -0700

    Add healthcheck endpoint.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get "/healthz" => "rails/health#show", as: :rails_health_check
+
   if Rails.env.development? or Rails.env.test?
     mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
   end
